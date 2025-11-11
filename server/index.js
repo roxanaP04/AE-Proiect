5ca4b95997a6ebc45b50b06417f241e86e40142d
roxanaP04 <petrescugabriela21@stud.ase.ro>
2025-11-11 14:45:45 +0200

    product

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const dotenv= require('dotenv');
 const morgan = require('morgan');
 const cors= require('cors')
-const { User } = require('./database/models/')
+const { User, Product } = require('./database/models/')
 
 const userRoutes= require('./routes/user.routes.js')
 const authRoutes= require('./routes/auth.routes.js')
+const productRoutes= require('./routes/product.routes.js')
 
 const app= express();
 dotenv.config();
@@ -18,6 +19,7 @@ app.use(express.json());
 
 app.use('/users', userRoutes);
 app.use('/auth', authRoutes);
+app.use('/products', productRoutes);
 
 app.get('/', (req, res) =>{
     res.status(200).json({ message: 'Hello' });
